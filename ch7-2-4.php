15f97438e76154c529403ef31de53fb03a70d341
Song1211-030 <C112181118@nkust.edu.tw>
2025-10-30 11:59:35 +0800

    34

---
+++ ch7-2-4.php
@@ -0,0 +1,13 @@
+<H1>SID: C112181118</H1>
+<BR>
+NAME: Song1211-030
+<P>
+<?php
+$weekday = array( 1=>"Mon", "Tue", "Wed", "Thu",
+                   "Fri", "Sat", "Sun");
+// 顯示陣列元素
+for ( $i = 1; $i <= count($weekday); $i++ )
+   echo "$i : $weekday[$i] ";
+echo "<br/>";?>
+</body>
+</html>
